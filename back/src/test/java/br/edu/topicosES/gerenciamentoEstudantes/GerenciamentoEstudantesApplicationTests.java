67470f2a90e80a30edd261609bd8dccfb4c829ec
Douglas Batista <86257838+douglasfsbatista@users.noreply.github.com>
2024-02-05 15:37:22 -0300

    Configurando docker e keycloak

---
+++ back/src/test/java/br/edu/topicosES/gerenciamentoEstudantes/GerenciamentoEstudantesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.topicosES.gerenciamentoEstudantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoEstudantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
